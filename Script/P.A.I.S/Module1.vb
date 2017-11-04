394de8f9b1e1925ba51700faee2c17bdab3797be
Paitorocxon <33005175+Paitorocxon@users.noreply.github.com>
2017-11-04 17:52:48 +0100

    Added MIT license to copyright()

--- Script/P.A.I.S/Module1.vb
+++ Script/P.A.I.S/Module1.vb
@@ -1712,6 +1712,28 @@ jumpot:
        "" & vbCrLf & _
        "" & vbCrLf & _
                           "")
+        Console.WriteLine("MIT License")
+        Console.WriteLine("")
+        Console.WriteLine("Copyright (c) 2017 Fabian Müller (aka Paitorocxon)")
+        Console.WriteLine("")
+        Console.WriteLine("Permission is hereby granted, free of charge, to any person obtaining a copy")
+        Console.WriteLine("of this software and associated documentation files (the ""Software""), to deal")
+        Console.WriteLine("in the Software without restriction, including without limitation the rights")
+        Console.WriteLine("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell")
+        Console.WriteLine("copies of the Software, and to permit persons to whom the Software is")
+        Console.WriteLine("furnished to do so, subject to the following conditions:")
+        Console.WriteLine("")
+        Console.WriteLine("The above copyright notice and this permission notice shall be included in all")
+        Console.WriteLine("copies or substantial portions of the Software.")
+        Console.WriteLine("")
+        Console.WriteLine("THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
+        Console.WriteLine("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+        Console.WriteLine("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+        Console.WriteLine("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
+        Console.WriteLine("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
+        Console.WriteLine("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE")
+        Console.WriteLine("SOFTWARE.")
+
 
         Console.ForegroundColor = DefForeColor
         Console.BackgroundColor = DefBackColor
